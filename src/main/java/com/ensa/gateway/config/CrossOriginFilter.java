eb2566f56f37ac176c234d9538b5e0ce2fdae57c
Abdelkhalak rbati <rbati.dev@gmail.com>
2023-02-05 00:27:06 +0100

    init project

---
+++ src/main/java/com/ensa/gateway/config/CrossOriginFilter.java
@@ -0,0 +1,26 @@
+package com.ensa.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CrossOriginFilter {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOriginPattern("http://localhost:*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
+
